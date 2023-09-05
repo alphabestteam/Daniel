e4fdd0f19eef9beb46fdbbba8354f2ad032b9b09
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2023-09-05 17:33:15 +0300

    added new functions to program

--- program.py
+++ program.py
@@ -1,9 +1,19 @@
 def main():
 
+    print(factorial(5))
+    print(factorial(6))
+    print(factorial(7))
+    print(factorial(8))
+    
     sum = 0
     for i in range(101):
         sum += i
     print(sum)
 
+def factorial(num):
+    fact = 1
+    for i in range(1, num + 1):
+        fact = fact * i
+    return fact
 if __name__ == "__main__":
     main()
