007fb30fd1929ec2cfec3ebecf508db97372f40f
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2023-09-19 13:02:36 +0300

    failed test. ; )

---
+++ Test_Python/main.py
@@ -0,0 +1,12 @@
+import datetime
+from developer import Developer
+from mission import Mission
+from project import Project
+# from developer import Developer
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
