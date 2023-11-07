9142d52e9ff72031696ab0bc8e555774e2ccbbca
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2023-11-07 13:29:24 +0200

    reformat and change name variables.js

--- js-exe/jsSandbox/main.js
+++ js-exe/jsSandbox/main.js
@@ -1,77 +1,65 @@
-// 1
+//3
 const mainHeading = document.getElementById("main-heading");
-
-// 2
+//4
 console.log(mainHeading.id);
-
-// 3
+//5
 console.log(mainHeading.className);
-
-// 4
+//6
 console.log(mainHeading.classList);
-
-// 5
+//7
 console.log(mainHeading.dataset);
-
-// 6
 console.log(mainHeading.getAttribute("nonstandard"));
-
-// 7
-mainHeading.classList.add("border");
-mainHeading.classList.add("bg-lightcyan"); 
-
-// 8
-console.log(mainHeading.textContent);
-
-// 9
-console.log(mainHeading.textContent.trim());
-
-// 10
-mainHeading.textContent = "Hello there, Pearl!"; 
-// 11
+//8
+mainHeading.classList.add("border", "bg-lightcyan");
+//9
+mainHeading.textContent;
+//10
+mainHeading.textContent.trim();
+//11
+mainHeading.textContent = "“Hello there pearl!";
+//12
 const span = document.createElement("span");
-span.innerText = "It's me, SpongeBob!"; 
-
-// 12
-console.log(mainHeading.textContent);
-
-// 13
+span.innerText = "its me SpongeBob!";
+//13
+mainHeading.textContent;
+//14
 const cloned = mainHeading.cloneNode(true);
 console.log(cloned);
-
-// 14
-const string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
-// 15
-let strArr = string.split(" ");
-
-// 16
+//15
+const subheading = document.createElement("h2");
+subheading.textContent = "jellyfish hunting is the best";
+//16
+document.body.appendChild(subheading);
+//17
+const string ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+//18
+const strArr = string.split(" ");
+console.log(strArr);
+//19
 const colors = [
   "red",
   "orange",
   "yellow",
   "greenyellow",
   "lightblue",
-  "mediumpurple"
+  "mediumpurple",
 ];
-
-// 17
+//20
 function randomColor() {
   const randomIndex = Math.floor(Math.random() * colors.length);
   return colors[randomIndex];
 }
+//21
 
-// 18
-const randomWordSection = document.getElementById("random-words");
-
-// 19
+const randomWord = document.getElementById("random-words");
+//22 & 23
 strArr.forEach((word) => {
-  const span = document.createElement("span");
-  const style = "background-color: " + randomColor();
-
-  span.setAttribute("style", style);
-  span.textContent = word;
-  span.className = "random-word";
-
-  randomWordSection.appendChild(span);
+    const span = document.createElement("span");
+    const style = "background-color: " + randomColor();
+  
+    span.setAttribute("style", style);
+    span.textContent = word;
+    span.className = "random-word";
+  
+    randomWord.appendChild(span);
 });
