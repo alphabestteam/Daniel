2fe27119e7becc1154a2dad248c7fb5c074195cc
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2023-11-07 13:35:26 +0200

    formatted code.

--- js-exe/files to use/script.js
+++ js-exe/files to use/script.js
@@ -1,20 +1,20 @@
-const button = document.getElementById('the-button');
+const button = document.getElementById("the-button");
 const main = document.querySelector("main");
 const bobGif = document.getElementById("bob");
 
-button.innerText = 'Show me bob ;)'
-bobGif.style.display = 'none';
+button.innerText = "Show me bob ;)";
+bobGif.style.display = "none";
 
-button.addEventListener("click", function() {
-    const showBob = button.getAttribute("data-show-bob");
+button.addEventListener("click", function () {
+  const showBob = button.getAttribute("data-show-bob");
 
-    if (showBob === "true") {
-        bobGif.style.display = "none";
-        button.innerText = "Show me bob ;)";
-        button.setAttribute("data-show-bob", "false");
-    } else {
-        bobGif.style.display = "block";
-        button.innerText = "Hide Bob!";
-        button.setAttribute("data-show-bob", "true");
-    }
+  if (showBob === "true") {
+    bobGif.style.display = "none";
+    button.innerText = "Show me bob ;)";
+    button.setAttribute("data-show-bob", "false");
+  } else {
+    bobGif.style.display = "block";
+    button.innerText = "Hide Bob!";
+    button.setAttribute("data-show-bob", "true");
+  }
 });
