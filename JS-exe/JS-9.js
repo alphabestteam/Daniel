1f839ccdc019919774f98f08a27fd579cefb71ea
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2023-11-07 12:45:24 +0200

    formatted code.

--- JS-exe/JS-9.js
+++ JS-exe/JS-9.js
@@ -1,4 +1,5 @@
-let text = "         Kung Fu Panda is a beloved animated movie about a clumsy, food-loving panda named Po who dreams of becoming a kung fu master.\nPo'sdream becomes a reality when he is unexpectedly chosen to become the Dragon Warrior and train with the Furious Five to protect the Valley of Peace from the evil Tai Lung.\nKung Fu Panda was released on June 6, 2008, and grossed over $631 million worldwide, making it the highest-grossing non-sequel animated film at the time of its release.\nAlong the way, Po learns valuable lessons about inner strength, perseverance, and the importance of family and friendship.\nWith stunning animation, a heartwarming story, and a star-studded cast including Jack Black, Angelina Jolie, and Jackie Chan, Kung Fu Panda has become a timeless classic for all ages"
+let text =
+  "         Kung Fu Panda is a beloved animated movie about a clumsy, food-loving panda named Po who dreams of becoming a kung fu master.\nPo'sdream becomes a reality when he is unexpectedly chosen to become the Dragon Warrior and train with the Furious Five to protect the Valley of Peace from the evil Tai Lung.\nKung Fu Panda was released on June 6, 2008, and grossed over $631 million worldwide, making it the highest-grossing non-sequel animated film at the time of its release.\nAlong the way, Po learns valuable lessons about inner strength, perseverance, and the importance of family and friendship.\nWith stunning animation, a heartwarming story, and a star-studded cast including Jack Black, Angelina Jolie, and Jackie Chan, Kung Fu Panda has become a timeless classic for all ages";
 // 1
 // const splitStr = (input) => {
 //     return input.split(" ");
@@ -25,20 +26,20 @@ let text = "         Kung Fu Panda is a beloved animated movie about a clumsy, f
 //     }
 //     console.log(toLower());
 //6
-    // const index = () =>{
-    //      return text.indexOf("Po");
-    // }
+// const index = () =>{
+//      return text.indexOf("Po");
+// }
 //     console.log(index());
 // 7
 //     const sliceText = () =>{
 //         return text.slice(index());
-        
+
 //     }
 //     console.log(sliceText());
 //8
 
-    // let result = text.trim();
-    // console.log(result);
+// let result = text.trim();
+// console.log(result);
 
 //9
 // const findAndExtract = (text) => {
@@ -53,19 +54,12 @@ let text = "         Kung Fu Panda is a beloved animated movie about a clumsy, f
 //   };
 //   console.log(extractWordsFromString(text));
 //11
-// const endsWithAges = text => {
-//     return text.endsWith("ages");
-//   };
-//   text1 = "This text ends with ages"
-//   console.log(endsWithAges(text1)); 
-  
+// const endsWithAges = text => text.endsWith("ages");
+// console.log(endsWithAges(text));
 //12
 // const addText = existingText => {
 //     const additionalText = " is one of my favorite movies!";
 //     return existingText + additionalText;
 //   };
-//   const newText = addText(text1);
-//   console.log(newText); 
-  
-
-  
+//   const newText = addText(text);
+//   console.log(newText);
