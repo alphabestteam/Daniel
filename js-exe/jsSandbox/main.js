815efcb2e78dbcab166e1ff377d3732668970480
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2023-11-07 13:34:36 +0200

    JS-14

--- js-exe/jsSandbox/main.js
+++ js-exe/jsSandbox/main.js
@@ -8,55 +8,50 @@ console.log(mainHeading.className);
 console.log(mainHeading.classList);
 
 console.log(mainHeading.dataset);
-
 console.log(mainHeading.getAttribute("nonstandard"));
 
-mainHeading.classList.add("border");
-mainHeading.classList.add("bg-lightcyan"); 
+mainHeading.classList.add("border", "bg-lightcyan");
 
-console.log(mainHeading.textContent);
+mainHeading.textContent;
 
+mainHeading.textContent.trim();
 
-console.log(mainHeading.textContent.trim());
-
-
-mainHeading.textContent = "Hello there, Pearl!"; 
+mainHeading.textContent = "“Hello there pearl!";
 
 const span = document.createElement("span");
-span.innerText = "It's me, SpongeBob!"; 
+span.innerText = "its me SpongeBob!";
 
-
-console.log(mainHeading.textContent);
-
+mainHeading.textContent;
 
 const cloned = mainHeading.cloneNode(true);
 console.log(cloned);
 
+const subheading = document.createElement("h2");
+subheading.textContent = "jellyfish hunting is the best";
 
-const string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+document.body.appendChild(subheading);
+
+const string ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+const strArr = string.split(" ");
+console.log(strArr);
 
 
-let strArr = string.split(" ");
-
-
-
-
-  
-
-const randomWordSection = document.getElementById("random-words");
+const randomWord = document.getElementById("random-words");
 
 strArr.forEach((word) => {
-  const span = document.createElement("span");
-  const style = "background-color: " + randomColor();
-
-  span.setAttribute("style", style);
-  span.textContent = word;
-  span.className = "random-word";
-
-  randomWordSection.appendChild(span);
+    const span = document.createElement("span");
+    const style = "background-color: " + randomColor();
+  
+    span.setAttribute("style", style);
+    span.textContent = word;
+    span.className = "random-word";
+  
+    randomWord.appendChild(span);
 });
 
-////////////////////////////14////////////////////////////////////////
+
+//////////////////////////// 14 ////////////////////////////////////////
 
 
 function randomColor() {
