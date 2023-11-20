2d7481e2677e3f051f6122295279dc19d7f0bc0c
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2023-11-20 17:03:03 +0200

    changed the way of changing alerts

--- basics-assignment-1-start/basics-assignment-1-start/src/app/app.component.ts
+++ basics-assignment-1-start/basics-assignment-1-start/src/app/app.component.ts
@@ -6,10 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  successCount: number = 0;
-  warningCount: number = 0;
+  count: number = 0;
   options: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10]; 
-
 
   getRange(count: number): Number[] {
     return Array(count);
