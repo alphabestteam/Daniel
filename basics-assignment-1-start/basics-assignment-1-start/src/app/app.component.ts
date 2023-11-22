94ac16ac35e151c49f737711c3000c8f05f8f545
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2023-11-22 11:44:26 +0200

    fix the given comments.

--- basics-assignment-1-start/basics-assignment-1-start/src/app/app.component.ts
+++ basics-assignment-1-start/basics-assignment-1-start/src/app/app.component.ts
@@ -3,20 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   count: number = 0;
-  options: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10]; 
+  options: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  isWarningAlert: boolean = false;
 
   getRange(count: number): Number[] {
     return Array(count);
-    
   }
 
-  displayed = false;
   changeAlert() {
-    this.displayed = !this.displayed;
+    this.isWarningAlert = !this.isWarningAlert;
   }
-
 }
